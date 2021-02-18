31f6f55f4a5412dcc6161011578251444f91e1e1
MaxHarlan206 <maximuscollinge@gmail.com>
2021-02-17 19:58:19 -0800

    still working?

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World, this is Max Harlan!'
+
+@app.route('/search_results')
+def search_results():
+    return 'Pretty neat search page'
