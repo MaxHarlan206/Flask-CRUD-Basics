9902f35a5bdefed8151f2a57bad079c2fe2f576d
MaxHarlan206 <maximuscollinge@gmail.com>
2021-02-17 22:09:42 -0800

    created database model

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQL_ALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db' # just by changing this one line later, we can use mysql or something like that which is more appropriate for production and enterprise
+db = SQLAlchemy(app)
+
+# this is the creation of a model, yes, you need to hardcode it, but is this such a big deal compared to django? no
+class BlogPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(140), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    author = db.Column(db.String(40), nullable=False, default="N/A")
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return 'Blog post' + str(self.id)
+
 all_posts = [
     {
        'title': 'post1',
