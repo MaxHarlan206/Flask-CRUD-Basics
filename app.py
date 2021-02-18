1b9e4a5dca0cf043b700ea345fe4e4e7d41b98cb
MaxHarlan206 <maximuscollinge@gmail.com>
2021-02-17 20:16:36 -0800

    beginning templates

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World, this is Max Harlan!'
+def index():
+    return render_template('index.html')
 
 @app.route('/search_results')
 def search_results():
     return 'Pretty neat search page'
+
+if __name__ == '__main__':
+    app.run(debug = True)
