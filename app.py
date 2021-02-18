e8bd22bac4b9f43b8dc1a3927a61e06cdc68909b
MaxHarlan206 <maximuscollinge@gmail.com>
2021-02-17 20:50:34 -0800

    accessing data, and dynamically generating html using python contorl flows

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-
+all_posts = [
+    {
+       'title': 'post1',
+       'content': 'this is the content of post 1',
+        'author': 'Max Harlan'
+    },
+    {
+        'title': 'post2',
+        'content': 'this is the content of post 2'
+    }
+]
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/posts')
+def posts():
+    return render_template('posts.html', posts=all_posts)
 
 @app.route('/search_results')
 def search_results():
