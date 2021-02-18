56069e96a497981ac80896974ccb65b63bddf4e0
MaxHarlan206 <maximuscollinge@gmail.com>
2021-02-18 10:31:16 -0800

    Delete functionality added

--- app.py
+++ app.py
@@ -38,6 +38,13 @@ def posts():
         all_posts = BlogPost.query.order_by(BlogPost.date_posted).all()
         return render_template('posts.html', posts=all_posts)
 
+@app.route('/posts/delete/<int:id>')
+def delete_post(id):
+    post = BlogPost.query.get_or_404(id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect('/posts')
+
 @app.route('/search_results')
 def search_results():
     return 'Pretty neat search page'
